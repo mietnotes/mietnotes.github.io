c6cd759b5f8b3eab398377e57f46532b573f4cf7
Tejus Gupta <tejus3131@gmail.com>
2023-05-17 18:09:35 +0530

    initial upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ContentComponent } from './content/content.component';
+import { FormComponent } from './form/form.component';
+import { FooterComponent } from './footer/footer.component';
+import { UpdateComponent } from './update/update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ContentComponent,
+    FormComponent,
+    FooterComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+  subjects = [
+    "BAS201",
+    "BAS202",
+    "BAS203",
+    "BAS204",
+    "BAS205",
+    "BCS201",
+    "BEC201",
+    "BEE201",
+    "BME201"
+  ]
+
+  selected_subject = 0
+
+  update_subject(e: any) {
+    this.selected_subject = e.target.value
+  }
+
+  units = [1,2,3,4,5]
+
+  selected_unit = 0
+
+  update_unit(e: any) {
+    this.selected_unit = e.target.value
+  }
+
+  download_notes() {
+    if (this.selected_subject && this.selected_unit) {
+      window.open("https://raw.githubusercontent.com/tejus3131/miethub/master/docs/assets/notes/"+this.selected_subject+"/"+this.selected_unit, '_blank')
+    }
+    else {
+      alert("Please select a subject and unit")
+    }
+  }
+
+}
