d0230ea7dcb02f523423af6617154a541dca20cf
Tejus Gupta <mietnotes31@gmail.com>
2023-06-06 16:11:31 +0530

    MietHub -> MietNotes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'miethub'`, () => {
+  it(`should have as title 'mietnotes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('miethub');
+    expect(app.title).toEqual('mietnotes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('miethub app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mietnotes app is running!');
   });
 });
